a93b130c76c684a4ab214f0b5b440c1331b4a43c
Евгений <jordan631@yandex.ru>
2022-06-05 00:36:20 +0400

    Added class Point2 and Test

---
+++ Tests/Graphics/Point2.cpp
@@ -0,0 +1,17 @@
+#include <Lt/Core/TestEqual.hpp>
+#include <Lt/Graphics/Point2i.hpp>
+
+int main()
+{
+	Lt::Graphics::Point2i point;
+
+	Lt_TestEqual(point.PosX() == 0);
+	Lt_TestEqual(point.PosY() == 0);
+
+	Lt::Graphics::Point2i point2(125, 385);
+
+	Lt_TestEqual(point2.PosX() == 125);
+	Lt_TestEqual(point2.PosY() == 385);
+
+	return 0;
+}
---
+++ src/Lt/Graphics/Point2i.hpp
@@ -0,0 +1,15 @@
+#ifndef Lt_Graphics_Point2i_hpp
+#define Lt_Graphics_Point2i_hpp
+
+#include <Lt/Core/Types.hpp>
+#include <Lt/Graphics/Point2.hpp>
+
+namespace Lt
+{
+	namespace Graphics
+	{
+		typedef Lt::Graphics::Point2<Lt::isize> Point2i;
+	}
+}
+
+#endif    
