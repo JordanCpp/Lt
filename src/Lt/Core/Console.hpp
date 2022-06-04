6320c92ef410334de24d71056b93f0cc1ac14896
Евгений <jordan631@yandex.ru>
2022-06-05 01:10:00 +0400

    Added #if (_WIN32 || _WIN64)

--- src/Lt/Core/Console.hpp
+++ src/Lt/Core/Console.hpp
@@ -1,6 +1,9 @@
 #ifndef Lt_Core_Console_hpp
 #define Lt_Core_Console_hpp
 
+#if (_WIN32 || _WIN64)
 #include <Lt/Platforms/Windows/Core/Console.hpp>
+#endif
+
 
 #endif    
--- src/Lt/Core/Terminate.hpp
+++ src/Lt/Core/Terminate.hpp
@@ -1,6 +1,8 @@
 #ifndef Lt_Core_Terminate_hpp
 #define Lt_Core_Terminate_hpp
 
+#if (_WIN32 || _WIN64)
 #include <Lt/Platforms/Windows/Core/Terminate.hpp>
+#endif
 
 #endif    
