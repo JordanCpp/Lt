acb4e03fdd3e25dad5517d76efb78cfe87fa6ebf
JordanCpp <jordan631@yandex.ru>
2022-06-10 13:40:37 +0400

    Refactoring tests for Memory

--- Tests/Core/Memory.cpp
+++ Tests/Core/Memory.cpp
@@ -1,12 +1,11 @@
 #include <Lt/Core/TestEqual.hpp>
 #include <Lt/Core/Memory.hpp>
 
-int main()
+void TestMemoryZero()
 {
 	const Lt::usize bytes = 1024;
 
 	char buffer[bytes];
-	char buffer2[bytes];
 
 	Lt::Memory::Zero(buffer, bytes);
 
@@ -14,20 +13,44 @@ int main()
 	{
 		LT_TEST_EQUAL(buffer[i] == '\0');
 	}
+}
 
-	Lt::Memory::Fill(buffer2, 'j', bytes);
+void TestMemoryFill()
+{
+	const Lt::usize bytes = 1024;
+
+	char buffer[bytes];
+
+	Lt::Memory::Fill(buffer, 'j', bytes);
 
 	for (Lt::usize i = 0; i < bytes; i++)
 	{
-		LT_TEST_EQUAL(buffer2[i] == 'j');
+		LT_TEST_EQUAL(buffer[i] == 'j');
 	}
+}
 
+void TestMemoryCopy()
+{
+	const Lt::usize bytes = 1024;
+
+	char buffer[bytes];
+	char buffer2[bytes];
+
+	Lt::Memory::Zero(buffer, bytes);
+	Lt::Memory::Fill(buffer2, 'j', bytes);
 	Lt::Memory::Copy(buffer, buffer2, bytes);
 
 	for (Lt::usize i = 0; i < bytes; i++)
 	{
 		LT_TEST_EQUAL(buffer[i] == 'j');
 	}
+}
+
+int main()
+{
+	TestMemoryZero();
+	TestMemoryFill();
+	TestMemoryCopy();
 
 	return 0;
 }
