7e779744ce2ee554cfe64327947fa1d0af8e8d82
JordanCpp <jordan631@yandex.ru>
2022-06-14 15:54:11 +0400

    Added StaticString

---
+++ Tests/Containers/StaticString.cpp
@@ -0,0 +1,66 @@
+#include <Lt/Core/TestEqual.hpp>
+#include <Lt/Containers/StaticString.hpp>
+
+void TestContainersStaticStringManual()
+{
+	const Lt::usize limit = 128;
+
+	Lt::Containers::StaticString<limit> string;
+
+	LT_TEST_EQUAL(string.Capacity() == limit - 1);
+	LT_TEST_EQUAL(string.Length() == 0);
+	LT_TEST_EQUAL(string.Content() != nullptr);
+}
+
+void TestContainersStaticStringAppendChar()
+{
+	const Lt::usize limit = 5;
+
+	Lt::Containers::StaticString<limit> string;
+
+	for (size_t i = 0; i < string.Capacity(); i++)
+	{
+		string.Append((char)i);
+
+		LT_TEST_EQUAL(string.Capacity() == limit - 1);
+		LT_TEST_EQUAL(string.Length() == i + 1);
+		LT_TEST_EQUAL(string.Content() != nullptr);
+		LT_TEST_EQUAL(string[i] == (char)i);
+	}
+}
+
+void TestContainersStaticStringAppendChars()
+{
+	const Lt::usize limit = 128;
+
+	Lt::Containers::StaticString<limit> string;
+
+	string.Append("Hello!");
+
+	LT_TEST_EQUAL(Lt::Chars::Equal(string.Content(), "Hello!"));
+}
+
+void TestContainersStaticStringClear()
+{
+	const Lt::usize limit = 128;
+
+	Lt::Containers::StaticString<limit> string;
+
+	string.Append("Hello!");
+
+	string.Clear();
+
+	LT_TEST_EQUAL(string.Capacity() == limit - 1);
+	LT_TEST_EQUAL(string.Length() == 0);
+	LT_TEST_EQUAL(string.Content() != nullptr);
+}
+
+int main()
+{
+	TestContainersStaticStringManual();
+	TestContainersStaticStringAppendChar();
+	TestContainersStaticStringAppendChars();
+    TestContainersStaticStringClear();
+
+	return 0;
+}
---
+++ src/Lt/Containers/StaticString.hpp
@@ -0,0 +1,88 @@
+#ifndef Lt_Containers_StaticString_hpp
+#define Lt_Containers_StaticString_hpp
+
+#include <Lt/Core/Chars.hpp>
+#include <Lt/Core/Assert.hpp>
+
+namespace Lt
+{
+	namespace Containers
+	{
+		template<Lt::usize COUNT>
+		class StaticString
+		{
+		public:
+			StaticString():
+				_Capacity(COUNT),
+				_Position(0)
+			{
+				LT_ASSERT(_Capacity >= 2);
+			}
+
+			Lt::usize Capacity()
+			{
+				return _Capacity - 1;
+			}
+
+			Lt::usize Length()
+			{
+				return _Position;
+			}
+
+			char * Content()
+			{
+				return _Content;
+			}
+
+			void Clear()
+			{
+				_Position = 0;
+			}
+
+			void Append(const char element)
+			{
+				if (_Position + 1 < _Capacity)
+				{
+					_Content[_Position] = element;
+
+					_Position++;
+
+					_Content[_Position] = '\0';
+				}
+			}
+
+			void Append(const char* elements)
+			{
+				Lt::usize i = 0;
+
+				while (elements[i] != '\0')
+				{
+					Append(elements[i]);
+
+					i++;
+				}
+			}
+
+			const char& operator[](Lt::usize index) const
+			{
+				LT_ASSERT(index <= _Capacity);
+
+				return _Content[index];
+			}
+
+
+			char& operator[](Lt::usize index)
+			{
+				LT_ASSERT(index <= _Capacity);
+
+				return _Content[index];
+			}
+		private:
+			Lt::usize _Capacity;
+			Lt::usize _Position;
+			char _Content[COUNT];
+		};
+	}
+}
+
+#endif    
