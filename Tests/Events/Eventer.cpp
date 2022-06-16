7e3eaad9fc9b15d57ec07033b98bd20b4f2d50c1
JordanCpp <jordan631@yandex.ru>
2022-06-16 14:05:57 +0400

    Update Eventer

--- Tests/Events/Eventer.cpp
+++ Tests/Events/Eventer.cpp
@@ -9,14 +9,34 @@ void TestEventsEventer()
 	LT_TEST_EQUAL(eventer.Running() == true);
 
 	Lt::Events::Event event;
+
 	event.Type = Lt::Events::IsQuit;
 	eventer.Push(event);
 	LT_TEST_EQUAL(eventer.Running() == true);
 
+	event.Type = Lt::Events::IsMouseClick;
+	eventer.Push(event);
+	LT_TEST_EQUAL(eventer.Running() == true);
+
+	event.Type = Lt::Events::IsMouseMove;
+	eventer.Push(event);
+	LT_TEST_EQUAL(eventer.Running() == true);
+
 	Lt::Events::Event dest;
+
 	LT_TEST_EQUAL(eventer.Pop(dest) == true);
 	LT_TEST_EQUAL(dest.Type == Lt::Events::IsQuit);
 	LT_TEST_EQUAL(eventer.Running() == true);
+
+	LT_TEST_EQUAL(eventer.Pop(dest) == true);
+	LT_TEST_EQUAL(dest.Type == Lt::Events::IsMouseClick);
+	LT_TEST_EQUAL(eventer.Running() == true);
+
+	LT_TEST_EQUAL(eventer.Pop(dest) == true);
+	LT_TEST_EQUAL(dest.Type == Lt::Events::IsMouseMove);
+	LT_TEST_EQUAL(eventer.Running() == true);
+
+	LT_TEST_EQUAL(eventer.Pop(dest) == false);
 
 	eventer.Stop();
 	LT_TEST_EQUAL(eventer.Running() == false);
