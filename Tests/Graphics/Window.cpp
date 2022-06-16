c991d61d3d9cc5628db060b42b08db64761c6370
JordanCpp <jordan631@yandex.ru>
2022-06-16 17:38:27 +0400

    Update test Window

--- Tests/Graphics/Window.cpp
+++ Tests/Graphics/Window.cpp
@@ -5,10 +5,25 @@ void TestGraphicsWindow()
 {
 	Lt::Core::ErrorHandler errorHandler;
 
-	Lt::Graphics::Window window(errorHandler, Lt::Graphics::Point2i(0, 0), Lt::Graphics::Point2i(800, 600), "Window!");
+	Lt::Graphics::Window window(errorHandler, Lt::Graphics::Point2i(25, 50), Lt::Graphics::Point2i(800, 600), "Window!");
 
 	LT_TEST_EQUAL(errorHandler.IsError() == false);
+	LT_TEST_EQUAL(window.Pos().PosX() == 25);
+	LT_TEST_EQUAL(window.Pos().PosY() == 50);
+	LT_TEST_EQUAL(window.Size().PosX() == 800);
+	LT_TEST_EQUAL(window.Size().PosY() == 600);
+	LT_TEST_EQUAL(Lt::Chars::Equal(window.Title(), "Window!"));
+
+	Lt::Events::Event event;
+
+	LT_TEST_EQUAL(window.GetEvent(event) == true);
+
+	window.StopEvent();
+
+	LT_TEST_EQUAL(window.GetEvent(event) == false);
+
 }
+
 int main()
 {
 	TestGraphicsWindow();
