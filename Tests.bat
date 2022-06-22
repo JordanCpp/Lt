1b6440e98ec149587d2a9b61e5ef89d38d491596
Jordan <jordan631@yandex.ru>
2022-06-22 12:40:27 +0300

    Fix Tests.bat

--- Tests.bat
+++ Tests.bat
@@ -1,14 +1,14 @@
 
-for %%f in (d:\Repos\Lt\out\build\x86-Debug\Tests\Allocators\*.exe) do call %%f
-for %%f in (d:\Repos\Lt\out\build\x86-Debug\Tests\Containers\*.exe) do call %%f
-for %%f in (d:\Repos\Lt\out\build\x86-Debug\Tests\Core\*.exe) do call %%f
-for %%f in (d:\Repos\Lt\out\build\x86-Debug\Tests\Events\*.exe) do call %%f
-for %%f in (d:\Repos\Lt\out\build\x86-Debug\Tests\Graphics\*.exe) do call %%f
+for %%f in (out\build\x86-Debug\Tests\Allocators\*.exe) do call %%f
+for %%f in (out\build\x86-Debug\Tests\Containers\*.exe) do call %%f
+for %%f in (out\build\x86-Debug\Tests\Core\*.exe) do call %%f
+for %%f in (out\build\x86-Debug\Tests\Events\*.exe) do call %%f
+for %%f in (out\build\x86-Debug\Tests\Graphics\*.exe) do call %%f
 
-for %%f in (d:\Repos\Lt\out\build\x64-Debug\Tests\Allocators\*.exe) do call %%f
-for %%f in (d:\Repos\Lt\out\build\x64-Debug\Tests\Containers\*.exe) do call %%f
-for %%f in (d:\Repos\Lt\out\build\x64-Debug\Tests\Core\*.exe) do call %%f
-for %%f in (d:\Repos\Lt\out\build\x64-Debug\Tests\Events\*.exe) do call %%f
-for %%f in (d:\Repos\Lt\out\build\x64-Debug\Tests\Graphics\*.exe) do call %%f
+for %%f in (out\build\x64-Debug\Tests\Allocators\*.exe) do call %%f
+for %%f in (out\build\x64-Debug\Tests\Containers\*.exe) do call %%f
+for %%f in (out\build\x64-Debug\Tests\Core\*.exe) do call %%f
+for %%f in (out\build\x64-Debug\Tests\Events\*.exe) do call %%f
+for %%f in (out\build\x64-Debug\Tests\Graphics\*.exe) do call %%f
 
 pause
