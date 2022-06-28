545897cfedaaa4aa27dd2df01ea30362f4bd5902
Jordan <jordan631@yandex.ru>
2022-06-28 18:55:37 +0300

    Add assert for CpuImage

--- src/Lt/Graphics/CpuImage.cpp
+++ src/Lt/Graphics/CpuImage.cpp
@@ -1,4 +1,5 @@
 #include <Lt/Graphics/CpuImage.hpp>
+#include <Lt/Core/Assert.hpp>
 
 Lt::Graphics::CpuImage::CpuImage() :
 	_Channels(4),
@@ -9,6 +10,9 @@ Lt::Graphics::CpuImage::CpuImage() :
 
 void Lt::Graphics::CpuImage::FromNew(const Lt::Graphics::Point2u& size)
 {
+	LT_ASSERT(size.PosX() > 0);
+	LT_ASSERT(size.PosY() > 0);
+
 	_Size = size;
 	_Allocator = nullptr;
 	_Pixels = nullptr;
@@ -19,12 +23,20 @@ void Lt::Graphics::CpuImage::FromNew(const Lt::Graphics::Point2u& size)
 }
 void Lt::Graphics::CpuImage::FromMemory(const Lt::Graphics::Point2u& size, Lt::u8* pixels)
 {
+	LT_ASSERT(size.PosX() > 0);
+	LT_ASSERT(size.PosY() > 0);
+	LT_ASSERT(pixels != nullptr);
+
 	_Size = size;
 	_Allocator = nullptr;
 	_Pixels = pixels;
 }
 void Lt::Graphics::CpuImage::FromAllocator(const Lt::Graphics::Point2u& size, Lt::Allocators::Allocator* allocator)
 {
+	LT_ASSERT(size.PosX() > 0);
+	LT_ASSERT(size.PosY() > 0);
+	LT_ASSERT(_Allocator != allocator);
+
 	_Size = size;
 	_Allocator = allocator;
 	_Pixels = nullptr;
