f7b0a4ade1bb32ae430d0110c6813d39c2d9fa77
Jordan <jordan631@yandex.ru>
2022-06-30 13:56:03 +0300

    Update Tests.bat

--- Tests.bat
+++ Tests.bat
@@ -4,11 +4,13 @@ for %%f in (out\build\x86-Debug\Tests\Containers\*.exe) do call %%f
 for %%f in (out\build\x86-Debug\Tests\Core\*.exe) do call %%f
 for %%f in (out\build\x86-Debug\Tests\Events\*.exe) do call %%f
 for %%f in (out\build\x86-Debug\Tests\Graphics\*.exe) do call %%f
+for %%f in (out\build\x86-Debug\Tests\Loaders\*.exe) do call %%f
 
 for %%f in (out\build\x64-Debug\Tests\Allocators\*.exe) do call %%f
 for %%f in (out\build\x64-Debug\Tests\Containers\*.exe) do call %%f
 for %%f in (out\build\x64-Debug\Tests\Core\*.exe) do call %%f
 for %%f in (out\build\x64-Debug\Tests\Events\*.exe) do call %%f
 for %%f in (out\build\x64-Debug\Tests\Graphics\*.exe) do call %%f
+for %%f in (out\build\x64-Debug\Tests\Loaders\*.exe) do call %%f
 
 pause
