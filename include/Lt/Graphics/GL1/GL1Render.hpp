45dc8e790c703c80990824faa4117b9a4cbc62e9
Jordan <jordan631@yandex.ru>
2022-07-01 15:51:09 +0300

    Update GL1Render

--- include/Lt/Graphics/GL1/GL1Render.hpp
+++ include/Lt/Graphics/GL1/GL1Render.hpp
@@ -3,6 +3,7 @@
 
 #include <Lt/Graphics/Base/BaseRender.hpp>
 #include <Lt/Graphics/GL1/GL1Window.hpp>
+#include <Lt/Graphics/Image.hpp>
 
 namespace Lt
 {
@@ -17,11 +18,17 @@ namespace Lt
 			void Clear();
 			void Color(const Lt::Graphics::Color& color);
 			void Present();
-			void Line(const Lt::Graphics::Point2u& pos1, const Lt::Graphics::Point2u& pos2);
+			Lt::Graphics::Image* Canvas();
+			void Pixel(const Lt::Graphics::Point2u& pos);
+			const Lt::Graphics::Color& GetPixel(const Lt::Graphics::Point2u& pos);
 			void FillRect(const Lt::Graphics::Point2u& pos, const Lt::Graphics::Point2u& size);
+			void Line(const Lt::Graphics::Point2u& pos1, const Lt::Graphics::Point2u& pos2);
+			Lt::u8* Pixels();
+			Lt::u8 Channels();
 		private:
 			Lt::Graphics::Window* _Window;
 			Lt::Graphics::BaseRender _BaseRender;
+			Lt::Graphics::Image _Canvas;
 		};
 	}
 }
--- src/Lt/Graphics/GL1/GL1Render.cpp
+++ src/Lt/Graphics/GL1/GL1Render.cpp
@@ -3,7 +3,8 @@
 
 Lt::Graphics::GL1Render::GL1Render(Lt::Core::ErrorHandler& errorHandler, Lt::Graphics::Window* window) :
 	_Window(window),
-	_BaseRender(_Window->Size())
+	_BaseRender(_Window->Size()),
+	_Canvas(_BaseRender.Size(), 4)
 {
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
@@ -38,6 +39,28 @@ void Lt::Graphics::GL1Render::Present()
 	_Window->Present();
 }
 
+Lt::Graphics::Image* Lt::Graphics::GL1Render::Canvas()
+{
+	return &_Canvas;
+}
+
+void Lt::Graphics::GL1Render::Pixel(const Lt::Graphics::Point2u& pos)
+{
+	glBegin(GL_POINTS);
+	glColor3f(1, 1, 1);
+	glVertex2i((GLint)pos.PosX(), (GLint)pos.PosY());
+	glEnd();
+}
+
+const Lt::Graphics::Color& Lt::Graphics::GL1Render::GetPixel(const Lt::Graphics::Point2u& pos)
+{
+	Lt::usize i = (Size().PosX() * pos.PosY()) + pos.PosX();
+
+	Lt::Graphics::Color* pixels = (Lt::Graphics::Color*)_Canvas.Pixels();
+
+	return	pixels[i];
+}
+
 void Lt::Graphics::GL1Render::Line(const Lt::Graphics::Point2u& pos1, const Lt::Graphics::Point2u& pos2)
 {
 	glBegin(GL_LINES);
@@ -55,4 +78,17 @@ void Lt::Graphics::GL1Render::FillRect(const Lt::Graphics::Point2u& pos, const L
 	glVertex2i((GLint)pos.PosX() + (GLint)size.PosX(), (GLint)pos.PosY());
 	glVertex2i((GLint)pos.PosX() + (GLint)size.PosX(), (GLint)pos.PosY() + (GLint)size.PosY());
 	glEnd();
+}
+
+Lt::u8* Lt::Graphics::GL1Render::Pixels()
+{
+	glPixelStorei(GL_PACK_ALIGNMENT, 1);
+	glReadPixels(0, 0, (GLsizei)_Canvas.Size().PosX(), (GLsizei)_Canvas.Size().PosY(), GL_RGBA, GL_UNSIGNED_BYTE, _Canvas.Pixels());
+
+	return _Canvas.Pixels();
+}
+
+Lt::u8 Lt::Graphics::GL1Render::Channels()
+{
+	return _Canvas.Channels();
 }
