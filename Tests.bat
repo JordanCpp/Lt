610af7ab33c5f2149beff78e46a441d900c6c4b0
Jordan <jordan631@yandex.ru>
2022-07-01 15:17:47 +0300

    Refactoring Graphics tests

--- Tests.bat
+++ Tests.bat
@@ -4,6 +4,9 @@ for %%f in (out\build\x86-Debug\Tests\Containers\*.exe) do call %%f
 for %%f in (out\build\x86-Debug\Tests\Core\*.exe) do call %%f
 for %%f in (out\build\x86-Debug\Tests\Events\*.exe) do call %%f
 for %%f in (out\build\x86-Debug\Tests\Graphics\*.exe) do call %%f
+for %%f in (out\build\x86-Debug\Tests\Graphics\Base\*.exe) do call %%f
+for %%f in (out\build\x86-Debug\Tests\Graphics\Cpu\*.exe) do call %%f
+for %%f in (out\build\x86-Debug\Tests\Graphics\GL1\*.exe) do call %%f
 for %%f in (out\build\x86-Debug\Tests\Loaders\*.exe) do call %%f
 for %%f in (out\build\x86-Debug\Tests\Managers\*.exe) do call %%f
 for %%f in (out\build\x86-Debug\Tests\Widgets\*.exe) do call %%f
@@ -14,6 +17,9 @@ for %%f in (out\build\x64-Debug\Tests\Containers\*.exe) do call %%f
 for %%f in (out\build\x64-Debug\Tests\Core\*.exe) do call %%f
 for %%f in (out\build\x64-Debug\Tests\Events\*.exe) do call %%f
 for %%f in (out\build\x64-Debug\Tests\Graphics\*.exe) do call %%f
+for %%f in (out\build\x64-Debug\Tests\Graphics\Base\*.exe) do call %%f
+for %%f in (out\build\x64-Debug\Tests\Graphics\Cpu\*.exe) do call %%f
+for %%f in (out\build\x64-Debug\Tests\Graphics\GL1\*.exe) do call %%f
 for %%f in (out\build\x64-Debug\Tests\Loaders\*.exe) do call %%f
 for %%f in (out\build\x64-Debug\Tests\Managers\*.exe) do call %%f
 for %%f in (out\build\x64-Debug\Tests\Widgets\*.exe) do call %%f
