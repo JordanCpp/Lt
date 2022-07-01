830d3d9f0c7d4ee1559a2510f5f66418bf9de923
Jordan <jordan631@yandex.ru>
2022-07-01 11:04:55 +0300

    Update Image

--- include/Lt/Graphics/Image.hpp
+++ include/Lt/Graphics/Image.hpp
@@ -1,6 +1,9 @@
 #ifndef Lt_Graphics_Image_hpp
 #define Lt_Graphics_Image_hpp
 
+#include <Lt/Core/Config.hpp>
+
+#if defined(LT_CONFIG_CPU_RENDER)
 #include <Lt/Graphics/CpuImage.hpp>
 
 namespace Lt
@@ -10,5 +13,17 @@ namespace Lt
 		typedef Lt::Graphics::CpuImage Image;
 	}
 }
+
+#elif defined(LT_CONFIG_GPU_RENDER)
+#include <Lt/Graphics/GpuImage.hpp>
+
+namespace Lt
+{
+	namespace Graphics
+	{
+		typedef Lt::Graphics::GpuImage Image;
+	}
+}
+#endif
 
 #endif    
