12f175aa992ed91c95271f8b68d5dcc2f9a1df22
Jordan <jordan631@yandex.ru>
2022-07-12 15:54:52 +0300

    Update CpuImage

---
+++ Tests/Graphics/Cpu/CpuImage.cpp
@@ -0,0 +1,37 @@
+#include <Lt/Core/TestEqual.hpp>
+#include <Lt/Graphics/Cpu/CpuImage.hpp>
+
+void TestGraphicsCpuImageInitNew()
+{
+	Lt::Graphics::CpuImage image(Lt::Graphics::Point2u(800, 600));
+
+	LT_TEST_EQUAL(image.Size().PosX() == 800);
+	LT_TEST_EQUAL(image.Size().PosY() == 600);
+	LT_TEST_EQUAL(image.Channels() == 4);
+	LT_TEST_EQUAL(image.Pixels() != nullptr);
+}
+
+void TestGraphicsCpuImageInitPixels()
+{
+	Lt::Graphics::Point2u size(800, 600);
+
+	Lt::u8* bytes = new Lt::u8[size.PosX() * size.PosY() * 4];
+
+	Lt::Graphics::CpuImage image(size, bytes);
+
+	LT_TEST_EQUAL(image.Size().PosX() == 800);
+	LT_TEST_EQUAL(image.Size().PosY() == 600);
+	LT_TEST_EQUAL(image.Channels() == 4);
+	LT_TEST_EQUAL(image.Pixels() != nullptr);
+	LT_TEST_EQUAL(image.Pixels() == bytes);
+
+	delete[] bytes;
+}
+
+int main()
+{
+	TestGraphicsCpuImageInitNew();
+	TestGraphicsCpuImageInitPixels();
+
+	return 0;
+}
