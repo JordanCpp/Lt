13901d9853eb65ca51a65e792d4b3b6aa394c0ec
Jordan <jordan631@yandex.ru>
2022-07-25 15:40:21 +0300

    Fix tests Sql

--- Tests.bat
+++ Tests.bat
@@ -11,6 +11,7 @@ for %%f in (out\build\x86-Debug\Tests\Loaders\*.exe) do call %%f
 for %%f in (out\build\x86-Debug\Tests\Managers\*.exe) do call %%f
 for %%f in (out\build\x86-Debug\Tests\Widgets\*.exe) do call %%f
 for %%f in (out\build\x86-Debug\Tests\Factories\*.exe) do call %%f
+for %%f in (out\build\x86-Debug\Tests\Sql\*.exe) do call %%f
 
 for %%f in (out\build\x64-Debug\Tests\Allocators\*.exe) do call %%f
 for %%f in (out\build\x64-Debug\Tests\Containers\*.exe) do call %%f
@@ -24,5 +25,6 @@ for %%f in (out\build\x64-Debug\Tests\Loaders\*.exe) do call %%f
 for %%f in (out\build\x64-Debug\Tests\Managers\*.exe) do call %%f
 for %%f in (out\build\x64-Debug\Tests\Widgets\*.exe) do call %%f
 for %%f in (out\build\x64-Debug\Tests\Factories\*.exe) do call %%f
+for %%f in (out\build\x64-Debug\Tests\Sql\*.exe) do call %%f
 
 pause
